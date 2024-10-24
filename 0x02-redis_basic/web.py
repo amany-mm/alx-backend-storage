705fcc6f760ebee51d04c2ec4096aa3a70d7855c
Amany Mohammed <amany.mohammd@gmail.com>
2024-10-24 04:58:03 +0300

    feat: Add 0x02-redis_basic

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Module containing function to return HTML content of a particular URL"""
+from typing import Callable
+from functools import wraps
+import requests
+import redis
+
+redis = redis.Redis()
+
+
+def count(method: Callable) -> Callable:
+    """ count the number of times a method is called """
+    @wraps(method)
+    def wrapper(url):
+        """ wrapper  of count """
+        redis.incr(f"count:{url}")
+        cached = redis.get(f"cached:{url}")
+        if cached:
+            return cached.decode('utf-8')
+        res = method(url)
+        redis.set(f"count:{url}", 0)
+        redis.setex(f"cached:{url}", 10, res)
+        return res
+    return wrapper
+
+
+@count
+def get_page(url: str) -> str:
+    """ get page of url"""
+    return requests.get(url, timeout=10).text
